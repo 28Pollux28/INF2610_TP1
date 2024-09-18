fa772345ab267fdc4db9d493c0ed78526959cba9
Valentin Lemaire <valema@l4714-16.info.polymtl.ca>
2024-09-18 11:34:58 -0400

    challenge 1

--- Section3_Challenges/challenges_part1.c
+++ Section3_Challenges/challenges_part1.c
@@ -3,15 +3,55 @@
  * Automne 2024
  * Challenges - part1.c
  *
- * Ajoutez vos noms, prénoms et matricules
+ * Valentin LEMAIRE 2409221
+ * Kamil MAARITE 2152653
 */
 #include "challenges_part1.h"
+#define ROOT_PATH "./root"
+
 
 int main(int argc, char*argv[])
 {
-    // TODO
+    char path[MAX_PATH_LENGTH]= ROOT_PATH;
+    DIR *d;
+    struct dirent *dir;
+    while(1){
+        label:
+        d = opendir(path);
+        if (d) {
+            printf("PATH: %s\nDID: %d\nPDID: %d\n",path,getpid(),getppid());
+            printf("FILES:\n");
+            while ((dir = readdir(d)) != NULL) {
+                if(dir->d_type == DT_DIR){
+                    if(strcmp(dir->d_name,".") == 0 || strcmp(dir->d_name,"..") == 0) continue; // Omit the "." and ".." folders
+                    if(fork()==0){
+                        strcat(path,"/");
+                        strcat(path,dir->d_name);
+                        goto label;
+                    }
+                    wait(NULL);
+                } else {
+
+                }
+                //printf("%s\n", dir->d_name);
+            
+            }
+            closedir(d);
+            if(strcmp(path,ROOT_PATH) != 0){
+                _exit(0);
+            }
+        }
+        break;
+        
+    }
+    
     return 0;
 }
 
 
 
+// pour chaque répertoire :
+// on dit l'emplacement du répertoire
+// on dit le n° de pid du processus
+// on dit le n° du processus parent
+// on liste tous les fichiers : si c'est un directory, on crée un nouveau processus et on recommence.
