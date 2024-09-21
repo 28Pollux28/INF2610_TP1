7f5e1d3080e55413d60ac5ff45bece47f61c301d
Valentin Lemaire <lemaire_valentin28@yahoo.fr>
2024-09-21 13:22:53 -0400

    challenge 2

--- Section3_Challenges/challenges_part2.c
+++ Section3_Challenges/challenges_part2.c
@@ -3,15 +3,39 @@
  * Automne 2024
  * Challenges - part2.c
  *
- * Ajoutez vos noms, prénoms et matricules
+ * Valentin LEMAIRE 2409221
+ * Kamil MAARITE 2152653
 */
 #include "challenges_part2.h"
 
 Matrix* multiply(Matrix* A, Matrix* B) {
-    
-    // TODO
+    // Vérifier la validité des matrices
+    if(A == NULL || B == NULL || A->cols != B->rows) {
+        return NULL;
+    }
+    // Créer la matrice résultat
+    Matrix* result = (Matrix*)malloc(sizeof(Matrix));
 
-    return (void*)1;
+    result->rows = A->rows;
+    result->cols = B->cols;
+    // Tableau 2D de taille ligne * colonne
+    // Allocation des lignes
+    result->matrix = (short**)malloc(sizeof(short*) * result->rows);
+    // Pour chaque ligne, allocation des colonnes
+    for(int i = 0; i < result->rows; i++) {
+        result->matrix[i] = (short*)malloc(sizeof(short) * result->cols);
+    }
+    // Calcul du produit
+    for(int i = 0; i < result->rows; i++) {
+        for(int j = 0; j < result->cols; j++) {
+            result->matrix[i][j] = 0;
+            for(int k = 0; k < A->cols; k++) {
+                result->matrix[i][j] += A->matrix[i][k] * B->matrix[k][j];
+            }
+        }
+    }
+
+    return result;
 }
 
 
@@ -19,6 +43,43 @@ Matrix* multiply(Matrix* A, Matrix* B) {
 // Don't forget to comment it out when you run the tests.
 // int main(int argc, char*argv[])
 // {
-   
+//     Matrix* A = (Matrix*)malloc(sizeof(Matrix));
+//     A->rows = 2;
+//     A->cols = 3;
+//     A->matrix = (short**)malloc(sizeof(short*) * A->rows);
+//     for(int i = 0; i < A->rows; i++) {
+//         A->matrix[i] = (short*)malloc(sizeof(short) * A->cols);
+//     }
+//     for(int i = 0; i < A->rows; i++) {
+//         for(int j = 0; j < A->cols; j++) {
+//             A->matrix[i][j] = i * A->cols + j;
+//         }
+//     }
+//     Matrix* B = (Matrix*)malloc(sizeof(Matrix));
+//     B->rows = 3;
+//     B->cols = 2;
+//     B->matrix = (short**)malloc(sizeof(short*) * B->rows);
+//     for(int i = 0; i < B->rows; i++) {
+//         B->matrix[i] = (short*)malloc(sizeof(short) * B->cols);
+//     }
+//     for(int i = 0; i < B->rows; i++) {
+//         for(int j = 0; j < B->cols; j++) {
+//             B->matrix[i][j] = i * B->cols + j;
+//         }
+//     }
+//     Matrix* C = multiply(A, B);
+//     for(int i = 0; i < C->rows; i++) {
+//         for(int j = 0; j < C->cols; j++) {
+//             printf("%d ", C->matrix[i][j]);
+//         }
+//         printf("\n");
+//     }
+
+//     free(A->matrix);
+//     free(A);
+//     free(B->matrix);
+//     free(B);
+//     free(C->matrix);
+//     free(C);
 //     return 0;
 // }
